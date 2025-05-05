c4c7e48c31b4c56db42ca86cbfb04f48a7702266
Kawtar Bouarfa <kbouarfa@c2r1p12.1337.ma>
2025-05-05 14:17:23 +0100

    ex00: modify list/tuple/set/dict greetings to match expected output

---
+++ ex00/Hello.py
@@ -0,0 +1,18 @@
+ft_list =["hello","tata!"]
+ft_list[1] = "World!"
+
+ft_tuple = ("hello", "toto!")
+ft_tuple = ("hello", "Morocco!")
+
+
+ft_set = {"hello", "tutu!"}
+ft_set.discard("tutu!")
+ft_set.add("Rabat!")
+
+ft_dict = {"hello" : "titi!"}
+ft_dict["hello"] = "1337Rabat"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
